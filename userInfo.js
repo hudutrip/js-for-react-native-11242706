84d8f7b626371553026be8b06d17eb35d37ae25f
hudutrip <126909852+hudutrip@users.noreply.github.com>
2024-06-25 17:00:48 +0000

    Add files via upload

---
+++ userInfo.js
@@ -0,0 +1,8 @@
+const { formatArrayStrings } = require('./arrayManipulation');
+function createUserProfiles(names, modifiedNames) {
+    return names.map((name, index) => ({
+        originalName: name,
+        modifiedName: modifiedNames[index],
+        id: index + 1
+    }));
+}
